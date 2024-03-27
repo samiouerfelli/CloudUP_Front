607f9353641a583630e737ec915dfa6f144d13e6
Salah <salah.elabed@esprit.tn>
2024-03-27 14:51:03 +0100

    Ajout de la page reclamation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { ReclamationComponent } from './reclamation/reclamation.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent, pathMatch: 'full' },
   { path: '', loadChildren: () => import('./all-modules/all-modules.module').then(m => m.AllModulesModule) },
+  { path: 'reclamation', component: ReclamationComponent},
   { path: '**', redirectTo: '', component: HomeComponent }
 ];
 
