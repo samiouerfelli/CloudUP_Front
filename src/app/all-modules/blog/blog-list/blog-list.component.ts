c8c0272593f25188be760cf1cb80e2e6c5ea7e01
salmaks <salma.ksantini@esprit.tn>
2024-04-30 18:30:46 +0100

    final

--- src/app/all-modules/blog/blog-list/blog-list.component.ts
+++ src/app/all-modules/blog/blog-list/blog-list.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import {forkJoin, merge, Observable, of, throwError} from 'rxjs';
+import { categories, Publication } from '../../../model/publication.model';
+import {catchError, map} from 'rxjs/operators';
+import { PublicationService } from '../../../service/publication.service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-blog-list',
@@ -6,10 +11,119 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./blog-list.component.css']
 })
 export class BlogListComponent implements OnInit {
+  categoriesCountMap: Map<string, number> = new Map<string, number>();
+  publication!: Observable<Publication[]>;
+  // tslint:disable-next-line:ban-types
+  errorMessage: Object | undefined;
+  allTags: string[] = [];
+  searchResults: Publication[] = [];
+  searchQuery = '';
+  selectedTags: string[] = [];
+  tagHovered: string | null = null;
 
-  constructor() { }
+
+  searchAndCategoryResults: Observable<Publication[]> | undefined;
+
+  // Déclaration de category en tant que membre de l'énumération Categories
+  private category!: categories;
+  private tags!: string;
+  private subject!: string;
+  private content!: string;
+  selectedCategory!: categories;
+  private categoryFilter: Observable<Publication[]> | undefined;
+  searchTags: Observable<Array<Publication>> | undefined;
+
+  constructor(private publicationService: PublicationService, private router: Router) {
+  }
 
   ngOnInit(): void {
+    this.publicationService.countCategoriesOccurences().subscribe((result: Map<string, number>) => {
+      this.categoriesCountMap = result;
+      this.getPublication();
+      this.getAllTags();
+    });
   }
+
+  getPublication(): void {
+    this.publication = this.publicationService.getPublications().pipe(
+      catchError(err => {
+        this.errorMessage = err.message;
+        return throwError(err);
+      })
+    );
+  }
+
+  getAllTags(): void {
+    this.allTags = [];
+    this.publication.subscribe(publications => {
+      publications.forEach((publication => {
+        // Vérifiez si la propriété tags est définie et n'est pas null
+        if (publication.tags) {
+          const tagsArray = publication.tags.split(' ');
+          tagsArray.forEach(tag => {
+            const trimmedTag = tag.trim();
+            if (!this.allTags.includes(trimmedTag)) {
+              this.allTags.push(trimmedTag);
+            }
+          });
+        }
+      }));
+    });
+  }
+
+  search(): void {
+    if (this.searchQuery.trim() !== '') {
+      const subjectResults$ = this.publicationService.searchBySubject(this.searchQuery);
+      const contentResults$ = this.publicationService.searchByContent(this.searchQuery);
+
+      forkJoin({ subjectResults: subjectResults$, contentResults: contentResults$ }).subscribe(results => {
+        const combinedResults = [...results.subjectResults, ...results.contentResults];
+        this.searchResults = combinedResults;
+      });
+    } else {
+      this.searchResults = [];
+    }
+  }
+  redirectToDetails(publication: Publication): void {
+    const idpub = publication.idpub;
+    console.log('l id de la pub récupérée:' + idpub);
+    // Rediriger vers la page de détails avec l'ID de la publication
+    this.router.navigate(['/blog/blog-details', idpub]);
+  }
+  toggleTag(tag: string): void {
+    const index = this.selectedTags.indexOf(tag);
+    if (index !== -1) {
+      // Si le tag est déjà sélectionné, le désélectionne
+      this.selectedTags.splice(index, 1);
+      this.searchResults = []; // Réinitialise les résultats de recherche
+    } else {
+      // Sinon, le sélectionne et effectue la recherche correspondante
+      this.selectedTags.push(tag);
+      this.publicationService.searchByTags(tag).subscribe(
+        (data) => {
+          this.searchResults = data;
+        },
+        (error) => {
+          console.error('Erreur lors de la recherche par tag :', error);
+        }
+      );
+    }
+  }
+
+
+
+  isTagSelected(tag: string): boolean {
+    return this.selectedTags.includes(tag) || this.tagHovered === tag;
+  }
+
+
+
+  filterByCategory(category: string): void {
+    this.categoryFilter = this.publication.pipe(
+      map(publications => publications.filter(p => p.categories.includes(category)))
+    );
+  }
+
+
 
 }
---
+++ src/app/service/commentary.service.ts
@@ -0,0 +1,71 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Commentary } from '../model/commentary.model';
+import {map} from "rxjs/operators";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CommentaryService {
+
+  constructor(private http: HttpClient) {}
+
+  addComment(idu: number, idpub: number, newComment: {
+    content: string;
+    tags: string;
+    // @ts-ignore
+    datePublication: Date,
+    solution: string;
+    voteNegatif: number;
+    votePositif: number;
+    username: string;
+    idpub: number;
+  }): Observable<number> {
+    return this.http.post<number>(`Http://localhost:8080/api/v1/auth/addCom/${idu}/${idpub}`, newComment);
+  }
+  searchByContent(idpub: number , content: string): Observable<Commentary[]> {
+    return this.http.get<Commentary[]>(`Http://localhost:8080/api/v1/auth/retrieveByContentC/${idpub}?content=` + content);
+  }
+
+  searchByTags(idpub: number, tags: string): Observable<Commentary[]> {
+    return this.http.get<Commentary[]>(`Http://localhost:8080/api/v1/auth/retrieveByTagsC/${idpub}?tags=` + tags);
+  }
+
+  likeComment(commentId: number): Observable<number> {
+    return this.http.put<number>(`Http://localhost:8080/api/v1/auth/${commentId}/upvote`, null);
+  }
+
+  dislikeComment(commentId: number): Observable<number> {
+    return this.http.put<number>(`Http://localhost:8080/api/v1/auth/${commentId}/downvote`, null);
+  }
+
+  getCommentLikes(commentId: number): Observable<number> {
+    return this.http.get<number>(`Http://localhost:8080/api/v1/auth/${commentId}/likes`);
+  }
+
+  getCommentDislikes(commentId: number): Observable<number> {
+    return this.http.get<number>(`Http://localhost:8080/api/v1/auth/${commentId}/dislikes`);
+  }
+
+  updateComment(comment: Commentary): Observable<Commentary> {
+    return this.http.put<Commentary>(`Http://localhost:8080/api/v1/auth/updateC`, comment);
+  }
+
+
+
+  deleteComment(commentId: number, publicationId: number): Observable<void> {
+    return this.http.delete<void>(`Http://localhost:8080/api/v1/auth/deleteC/${commentId}/${publicationId}`);
+  }
+  checkAndMarkSolution(): Observable<boolean> {
+    return this.http.put<boolean>('Http://localhost:8080/api/v1/auth/mark-solution-and-close', {});
+  }
+  getCommentsName(userId: number): Observable<string> {
+    // @ts-ignore
+    return this.http.get<string>(`http://localhost:8080/api/v1/auth/comments?userId=${userId}`, { responseType: 'text' });
+  }
+
+
+
+
+}
