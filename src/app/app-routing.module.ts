c1904fb01084206b5e9cdbc73f0c9c8db458c14c
salmaks <salma.ksantini@esprit.tn>
2024-04-30 18:31:53 +0100

    final

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import {BlogDetailsComponent} from './all-modules/blog/blog-details/blog-details.component';
+import {BlogListComponent} from "./all-modules/blog/blog-list/blog-list.component";
 
 const routes: Routes = [
   { path: '', component: HomeComponent, pathMatch: 'full' },
+  { path: 'blog/blog-details/:idpub', component: BlogDetailsComponent },
   { path: '', loadChildren: () => import('./all-modules/all-modules.module').then(m => m.AllModulesModule) },
-  { path: '**', redirectTo: '', component: HomeComponent }
+  { path: '**', redirectTo: '', component: HomeComponent }, // Placer la route de capture à la fin
 ];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
